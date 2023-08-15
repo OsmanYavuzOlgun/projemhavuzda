24cf13a06991e49bc467126b06215b4d6bd3d76d
Osman Yavuz Olgun <osmanyavuzolgun@gmail.com>
2023-08-15 06:24:20 +0300

    projemhavuzda :: added pages

--- main.js
+++ main.js
@@ -14,3 +14,36 @@ function decreaseValue() {
     input.value = value - 1;
   }
 }
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+var avFiles = document.getElementById("available-files");
+var buyD = document.getElementById("buy-domain");
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    this.classList.toggle("border-bottom-v2")
+   
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+      avFiles.classList.remove("blockdisplay");
+      buyD.classList.remove("flexdisplay");
+    } else {
+      avFiles.classList.add("blockdisplay");
+      buyD.classList.add("flexdisplay");
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+const slider = document.getElementById("myRange");
+const sliderFill = document.querySelector(".slider-fill");
+
+slider.addEventListener("input", function() {
+  const percent = (slider.value - slider.min) / (slider.max - slider.min) * 100;
+  sliderFill.style.width = percent + "%";
+});
