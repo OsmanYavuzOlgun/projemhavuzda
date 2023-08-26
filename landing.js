7a8396fe5b4d662187f27a8aecd465a92954250a
Osman Yavuz Olgun <osmanyavuzolgun@gmail.com>
2023-08-26 05:11:22 +0300

    projemhavuzda :: landing done without login and register

---
+++ landing.js
@@ -0,0 +1,113 @@
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+const sections = document.querySelectorAll("section[id]");
+
+
+var lheader = document.getElementById("header");
+
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  if (this.scrollY >= 200) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+
+  if (this.scrollY >= 60) lheader.classList.add("bg-change");
+  else lheader.classList.remove("bg-change");
+}
+window.addEventListener("scroll", scrollHeader);
+
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
+  if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollTop);
+
+
+
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-toggle-right";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme)
+    ? "bx-toggle-left"
+    : "bx-toggle-right";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "bx-toggle-left" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute("aria-expanded");
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute("aria-expanded", "false");
+  }
+
+  if (itemToggle == "false") {
+    this.setAttribute("aria-expanded", "true");
+  }
+}
+
+items.forEach((item) => item.addEventListener("click", toggleAccordion));
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+const sr = ScrollReveal({
+  distance: "30px",
+  duration: 1800,
+  reset: true,
+});
+
+sr.reveal(
+  `.home__data, .home__img, 
+             .decoration__data,
+             .accessory__content,
+             .footer__content`,
+  {
+    origin: "top",
+    interval: 200,
+  }
+);
+
+sr.reveal(`.share__img, .send__content`, {
+  origin: "left",
+});
+
+sr.reveal(`.share__data, .send__img`, {
+  origin: "right",
+});
